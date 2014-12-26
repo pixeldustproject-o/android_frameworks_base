d54f947154be1e5b6ac1a119f1b78a2e2a798b46
chunshu liu <chunshu.x.liu@sonymobile.com>
2014-12-26 11:14:20 +0900

    Turn LCD off when mirroring display to save power
    registered as service, getSystemsService() then call it to turn lcd off
    
    Test: mirror screen to TV, phone lcd is off
    
    Change-Id: I690275d126afbf8ab56b608959a32343fba91f12
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- core/java/android/app/SystemServiceRegistry.java
+++ core/java/android/app/SystemServiceRegistry.java
@@ -150,6 +150,8 @@ import com.android.internal.app.IAppOpsService;
 import com.android.internal.app.IBatteryStats;
 import com.android.internal.app.ISoundTriggerService;
 import com.android.internal.appwidget.IAppWidgetService;
+import com.android.internal.mirrorpowersave.ILcdPowerSave;
+import com.android.internal.mirrorpowersave.LcdPowerSaveManager;
 import com.android.internal.os.IDropBoxManagerService;
 import com.android.internal.policy.PhoneLayoutInflater;
 
@@ -907,6 +909,15 @@ final class SystemServiceRegistry {
             @Override
             public RulesManager createService(ContextImpl ctx) {
                 return new RulesManager(ctx.getOuterContext());
+            }});
+
+        registerService(Context.LCD_POWER_SAVE_SERVICE, LcdPowerSaveManager.class,
+                new CachedServiceFetcher<LcdPowerSaveManager>() {
+            @Override
+            public LcdPowerSaveManager createService(ContextImpl ctx) {
+                IBinder b = ServiceManager.getService(Context.LCD_POWER_SAVE_SERVICE);
+                ILcdPowerSave service = ILcdPowerSave.Stub.asInterface(b);
+                return new LcdPowerSaveManager(ctx.getOuterContext(), service);
             }});
     }
 
