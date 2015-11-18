b126f7e6925103698c0e3ef86dc14658314994e6
maxwen <max.weninger@gmail.com>
2015-11-18 21:43:53 +0100

    base: unlink notification volume is back [1/2]
    -delete old unused volume images
    -add new one for notification volumes
    maybe some day one will be able to create a vector
    drawable for this - I am not :)
    
    base: add vector for notification volume mute
    
    thanks to Marko Man
    
    Change-Id: I85f63ecb5b8c4cc359c8ffeb625a582c67354917
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4244,6 +4244,13 @@ public final class Settings {
         public static final String OMNIJAWS_WEATHER_ICON_PACK = "omnijaws_weather_icon_pack";
 
         /**
+         * Boolean value whether to link ringtone and notification volume
+         *
+         * @hide
+         */
+        public static final String VOLUME_LINK_NOTIFICATION = "volume_link_notification";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -4417,6 +4424,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(QS_LAYOUT_COLUMNS_LANDSCAPE);
             PRIVATE_SETTINGS.add(QS_TILE_TITLE_VISIBILITY);
             PRIVATE_SETTINGS.add(QS_LAYOUT_ROWS);
+            PRIVATE_SETTINGS.add(VOLUME_LINK_NOTIFICATION);
         }
 
         /**
