4428a684db6b8911d17c19d05669248434f418af
eyosen <abittin@gmail.com>
2015-12-05 18:36:19 +0100

    Add Screen Record to the Power Menu [1/2]
    Change-Id: I859161fb82d899c579dff1bb35e85a47f9da4002
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4421,6 +4421,13 @@ public final class Settings {
         public static final String POWERMENU_TORCH = "powermenu_torch";
 
         /**
+         * Whether to display the screenrecord option in the power menu
+         *
+         * @hide
+         */
+        public static final String POWERMENU_SCREENRECORD = "powermenu_screenrecord";
+
+        /**
          * Whether to skip music track with volume rocker
          * @hide
          */
