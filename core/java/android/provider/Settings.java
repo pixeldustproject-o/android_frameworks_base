b12fcfba3096e2516c633d9788b71477e2e8f9e9
Alex Cruz <mazdarider23@gmail.com>
2016-01-03 04:07:47 +0000

    Lockscreen Clock Widget Fonts [1/2]
    This is beanstown106's idea, we got talking while messing around with some layouts
    and decided that this should be a mod and so here it is :-)
    
    Currently there are 12 fonts to include default font, will add more as time goes by
    
    http://i.imgur.com/adKTfsM.png
    
    http://i.imgur.com/XyaACLw.jpg
    
    BenzoEdit:
    Fixed the fonts being tiny
    
    Change-Id: I3dccd912166643645b2b97017a7a267649ab6538
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4739,6 +4739,13 @@ public final class Settings {
         public static final String SYSTEM_THEME_STYLE = "system_theme_style";
 
         /**
+         * Change fonts for the system lockscreen clock widget
+         *
+         * @hide
+         */
+        public static final String LOCK_CLOCK_FONTS = "lock_clock_fonts";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
