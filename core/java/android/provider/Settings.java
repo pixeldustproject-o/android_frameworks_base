8d1d442d53d3969a3177d043590f1f4ece4f33d1
maxwen <max.weninger@gmail.com>
2016-03-18 18:17:05 +0100

    Volume key music control [1/2]
    Change-Id: I3b1e72b95ec8b1b63ef0219259dfee77a5d339d3
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4333,6 +4333,12 @@ public final class Settings {
          */
         public static final String ALERT_ON_CHARGED_LEVEL = "alert_on_charged_level";
 
+        /** Whether to skip music track with volume rocker
+        /**
+         * @hide
+         */
+        public static final String VOLUME_BUTTON_MUSIC_CONTROL = "volume_button_music_control";
+
         /**
          * Volume rocker wake
          * @hide
@@ -4516,6 +4522,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(SWAP_NAVIGATION_KEYS);
             PRIVATE_SETTINGS.add(VOLUME_LINK_NOTIFICATION);
             PRIVATE_SETTINGS.add(WAKE_WHEN_PLUGGED_OR_UNPLUGGED);
+            PRIVATE_SETTINGS.add(VOLUME_BUTTON_MUSIC_CONTROL);
         }
 
         /**
