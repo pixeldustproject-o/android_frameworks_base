ec12d6df19329b820f5fb7bfe2a1556398dd3829
Steve Kondik <steve@cyngn.com>
2016-07-29 11:54:24 -0700

    mountservice: Don't nuke all volumes when decrypting
     * Instead, just unmount the emulated internal volume since
       it's the only one which can prevent cryptfs from restarting.
    
    Change-Id: I757babaf763b4ef789b165d116da0708a3530e99
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- services/core/java/com/android/server/StorageManagerService.java
+++ services/core/java/com/android/server/StorageManagerService.java
@@ -2645,7 +2645,6 @@ class StorageManagerService extends IStorageManager.Stub
                 mHandler.postDelayed(new Runnable() {
                     public void run() {
                         try {
-                            mConnector.execute("volume", "shutdown");
                             mCryptConnector.execute("cryptfs", "restart");
                         } catch (NativeDaemonConnectorException e) {
                             Slog.e(TAG, "problem executing in background", e);
