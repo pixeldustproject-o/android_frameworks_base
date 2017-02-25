bb313d06fd07f3e538a1ee6b209931429fa50e95
maxwen <max.weninger@gmail.com>
2017-02-25 17:31:05 +0100

    SystemUI: OmniJaws qs tile integration
    Change-Id: I4a00255a362298820f9f7997b3089e135358e0f6
    
    base: qs weather: be nice to people without OmniJaws
    
    if OmniJaws is not available or disabled show an informative
    Toast. Also change the default tile image to show also some sun :)
    
    Change-Id: Id1216dfc2aa5a9cf86e1b6accaf354f25c5541fd
    
    base: SystemUI: remove force weather update bits
    
    force updates have been removed from service API
    
    Change-Id: I183bb1a3990d28211d99ff024f9faf59faa6ac4b
    
    base: SystemUI: use new OmniJaws error broadcasts
    
    Change-Id: I3c7da3843186897b7c93d477ca0696a39e0ada65
    
    base: SystemUI: dont hide weather forcast data on errors
    
    only show error in header
    see https://gerrit.omnirom.org/23116 for details
    
    Change-Id: Iaa2fa7b6e0d6638b142b19149c5b647d64f3b2e0
    
    base: SystemUI: dont show OmniJaws error that prominent
    
    instead just show a small * to indicate an error. In best case
    the update has been triggered on screen on anyway and will
    be updated soon
    
    Change-Id: I31446c8339e8093e41f9fc8a2788b251bcd3dced
    
    base: SystemUI: add dedicated TextView for OmniJaws error marker
    
    dont mess with the layout in the error case
    and fix proper layout handling for loooong city names
    
    mydongistiny: moved OmniJawsClient over to core/java/com/android/internal/util/pixeldust
    	so there's only need for one client instead of a new one because of the keyguard
    Change-Id: I52919421cbb550e2a662e7df65118d4b03dcebf4
    
    PixelDustEdits: Updated for 8.1
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -43,6 +43,7 @@ import com.android.systemui.qs.tiles.ScreenrecordTile;
 import com.android.systemui.qs.tiles.ScreenshotTile;
 import com.android.systemui.qs.tiles.SyncTile;
 import com.android.systemui.qs.tiles.UserTile;
+import com.android.systemui.qs.tiles.WeatherTile;
 import com.android.systemui.qs.tiles.WifiTile;
 import com.android.systemui.qs.tiles.WorkModeTile;
 import com.android.systemui.qs.QSTileHost;
@@ -79,6 +80,7 @@ public class QSFactoryImpl implements QSFactory {
         else if (tileSpec.equals("sync")) return new SyncTile(mHost);
         else if (tileSpec.equals("navbar")) return new NavBarTile(mHost);
         else if (tileSpec.equals("screenrecord")) return new ScreenrecordTile(mHost);
+        else if (tileSpec.equals("weather")) return new WeatherTile(mHost);
         // Intent tiles.
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(mHost, tileSpec);
         else if (tileSpec.startsWith(CustomTile.PREFIX)) return CustomTile.create(mHost, tileSpec);
