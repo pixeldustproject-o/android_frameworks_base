e3d48cc30bbb53346dfa5511903b3e99f12e3bfe
LorDClockaN <lordclockan@gmail.com>
2017-03-06 22:32:58 +0100

    WeatherTile: Start Google Weather on LongClick
    Change-Id: I049e026bcdcdea34ed61a07e6de953d91c6e57c1
    
    PixelDust Edits: update PackageUtils.
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- core/java/com/android/internal/util/pixeldust/PackageUtils.java
+++ core/java/com/android/internal/util/pixeldust/PackageUtils.java
@@ -15,11 +15,19 @@
 */
 package com.android.internal.util.pixeldust;
 
+import android.app.ActivityManager;
 import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.util.Log;
+
+import java.util.List;
 
 public class PackageUtils {
+
+    private static final String TAG = "PackageUtils";
 
     public static boolean isAppInstalled(Context context, String appUri) {
         try {
@@ -42,5 +50,65 @@ public class PackageUtils {
         } catch (NameNotFoundException e) {
             return false;
         }
+    }
+
+   /**
+     * Checks if a specific package is installed.
+     *
+     * @param context     The context to retrieve the package manager
+     * @param packageName The name of the package
+     * @return Whether the package is installed or not.
+     */
+    public static boolean isPackageInstalled(Context context, String packageName) {
+        PackageManager pm = context.getPackageManager();
+        try {
+            if (pm != null) {
+                List<ApplicationInfo> packages = pm.getInstalledApplications(0);
+                for (ApplicationInfo packageInfo : packages) {
+                    if (packageInfo.packageName.equals(packageName)) {
+                        return true;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "Error: " + e.getMessage());
+        }
+        return false;
+    }
+
+    public static boolean isPackageEnabled(String packageName, PackageManager pm) {
+        try {
+            ApplicationInfo ai = pm.getApplicationInfo(packageName, 0);
+            return ai.enabled;
+        } catch (PackageManager.NameNotFoundException notFound) {
+            return false;
+        }
+    }
+
+    /**
+     * Checks if a specific service is running.
+     *
+     * @param context     The context to retrieve the activity manager
+     * @param serviceName The name of the service
+     * @return Whether the service is running or not
+     */
+    public static boolean isServiceRunning(Context context, String serviceName) {
+        ActivityManager activityManager = (ActivityManager) context
+                .getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningServiceInfo> services = activityManager
+                .getRunningServices(Integer.MAX_VALUE);
+
+        if (services != null) {
+            for (ActivityManager.RunningServiceInfo info : services) {
+                if (info.service != null) {
+                    if (info.service.getClassName() != null && info.service.getClassName()
+                            .equalsIgnoreCase(serviceName)) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
     }
 }
