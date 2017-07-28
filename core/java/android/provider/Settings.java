0fdd1535297d4c624c5262236ef54f909dc660c4
Ivan Iskandar <iiiiskandar14@gmail.com>
2017-07-28 04:56:45 +0200

    OMS: base: Introduce SubstratumService
    commit f9076ddbb95a5871ef5827059aa5e04a3b6df19e
    Author: Ivan Iskandar <iiiiskandar14@gmail.com>
    Date:   Fri Jul 28 04:56:45 2017 +0200
    
        base: Introduce SubstratumService
    
        This change deprecates ThemeInterfacer, so for testers, please do not
        compile ThemeInterfacer with this change!
    
        Change-Id: I8c9f331ca7e4c68c35ea13bed587775ee4abeb30
        Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>
        Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    
    commit 182201c208c3dffe79a9abfda2777e8024058a82
    Author: mickybart <mickybart@pygoscelis.org>
    Date:   Sat Nov 19 19:05:05 2016 -0500
    
        OMS: StrictMode and files under /data/system/theme/
    
        Themes are using /data/system/theme/ to push some files like LowBattery.ogg (audio notification)
        When the device battery trigger the low battery state, the sound is not played due
        to StrictMode and SystemUI is crashing.
    
        So we need that StrictMode authorize files under /system OR /data/system/theme
    
        Logcat of the issue:
    
        E AndroidRuntime: Caused by: android.os.FileUriExposedException: file:///data/system/theme/audio/ui/LowBattery.ogg exposed beyond app through Notification.sound
        E AndroidRuntime:        at android.os.StrictMode.onFileUriExposed(StrictMode.java:1799)
        E AndroidRuntime:        at android.net.Uri.checkFileUriExposed(Uri.java:2346)
        E AndroidRuntime:        at android.app.NotificationManager.notifyAsUser(NotificationManager.java:300)
    
        Change-Id: I154dc4280de8eaf891772a9632283e9f547f5718
        (cherry picked from commit 838f6466d39a100f9709ac253a6d7358ca66829f)
        Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    
    commit baab8f675f2237088b02177d942fecef0e598232
    Author: George G <kreach3r@users.noreply.github.com>
    Date:   Sat Mar 11 14:11:54 2017 +0200
    
        Hold "volume up" during boot to disable all overlays
    
        Following the way "safe mode" was coded, you can now long press
        "volume up" during boot to automatically disable all overlays
        (from the current system/owner user).
    
        This should come in handy as a global "reset" mechanism.
    
        Example output:
        03-12 03:22:07.090   678   678 D TEST    : Disabling overlay android.GalaxyEvolution
        03-12 03:22:07.176   678   678 D TEST    : Disabling overlay com.android.launcher3.GalaxyEvolution
        03-12 03:22:07.267   678   678 D TEST    : Disabling overlay com.android.server.telecom.GalaxyEvolution
    
        Caveats:
        SystemServer seems to have already loaded a context based on the
        overlays by the time "disableOverlays" is finished, so SystemUI
        turns up themed, even if the overlay is correctly disabled.
    
        In the case of a user using this reset mode to "fix" a SystemUI FC,
        this means that the user will experience one more FC after boot
        (which then would trigger a SystemUI restart, loading the default
        theme this time, and thus ending the FC loop).
    
        Change-Id: I64fb769ea175d37a90a0804f916926b63e5b93ca
        Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    
    commit 2b946710a055b8d9921104f1bc83b173a7eb1d4f
    Author: Ivan Iskandar <iiiiskandar14@gmail.com>
    Date:   Thu Mar 9 13:28:22 2017 +0100
    
        ThemeSafety: Introduce App Crash Intent
    
        The intent received by substratum and it will disable all enabled
        overlays.
    
        Change-Id: Ifabd57c2ea71ca93ecc2959ce09ccde3e91782dd
        Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    
    commit 95135bdf709e1e11af24bd5ba0740403535596b0
    Author: George G <kreach3r@users.noreply.github.com>
    Date:   Mon Jul 4 06:25:15 2016 +0300
    
        ApplicationsState: add filter for Substratum overlays [2/2]
    
        This commit allows the framework to handle the filtering of the
        overlays found for OMS.
    
        Change-Id: I7646115e8f73494d726728fac58cc47aafd69d5d
        Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    
    Change-Id: Ic17cca331d8615feab27aafdb5ad9a31e0a01760
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7434,6 +7434,13 @@ public final class Settings {
         public static final String STATUS_BAR_LOCKED_ON_SECURE_KEYGUARD = "status_bar_locked_on_secure_keyguard";
 
         /**
+         * Force authorize Substratum (or equivalent) frontend calling packages by ThemeInterfacer
+         * The value is boolean (1 or 0).
+         * @hide
+         */
+        public static final String FORCE_AUTHORIZE_SUBSTRATUM_PACKAGES = "force_authorize_substratum_packages";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
