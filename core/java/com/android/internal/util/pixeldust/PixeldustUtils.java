586b5879a9b4eedf8d2842c00237dc29947c0722
bigrushdog <randall.rushing@gmail.com>
2017-09-06 20:46:51 +0200

    Add api to take screenshots
    abc: extracted from DUI
    add a public call to PixeldustUtils
    
    Change-Id: If71944466c703a9b3fe54aea6bb21150b0a082bb
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- core/java/com/android/internal/util/pixeldust/PixeldustUtils.java
+++ core/java/com/android/internal/util/pixeldust/PixeldustUtils.java
@@ -16,11 +16,14 @@
 package com.android.internal.util.pixeldust;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.PowerManager;
 import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.os.SystemClock;
+import android.view.IWindowManager;
+import android.view.WindowManagerGlobal;
 
 import com.android.internal.statusbar.IStatusBarService;
 
@@ -28,6 +31,9 @@ import com.android.internal.statusbar.IStatusBarService;
  * Some custom utilities
  */
 public class PixeldustUtils {
+
+    public static final String INTENT_SCREENSHOT = "action_take_screenshot";
+    public static final String INTENT_REGION_SCREENSHOT = "action_take_region_screenshot";
 
     public static void switchScreenOff(Context ctx) {
         PowerManager pm = (PowerManager) ctx.getSystemService(Context.POWER_SERVICE);
@@ -42,6 +48,15 @@ public class PixeldustUtils {
 
     public static void toggleCameraFlash() {
         FireActions.toggleCameraFlash();
+    }
+
+    public static void takeScreenshot(boolean full) {
+        IWindowManager wm = WindowManagerGlobal.getWindowManagerService();
+        try {
+            wm.sendCustomAction(new Intent(full? INTENT_SCREENSHOT : INTENT_REGION_SCREENSHOT));
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
     }
 
     private static final class FireActions {
