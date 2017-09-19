c3b4d2c96199613418df4e864ce581f314deda4e
Alex Cruz <mazdarider23@gmail.com>
2017-09-19 19:15:26 +0800

    Pimp the power menu out [1/2]
    xyyx
    -reworked for O
    -used ezio84 method to screenshot(one tap - full, log tap - partial)
    -added toggle for users
    -lock, screenshot, settings icons replaced with Android Studio vectors
    -add delay to screenshot action and hide power dialog when partial
    screenshot
    
    PixelDust Edits:
    -aligned to work with maxwen's advanced reboot menu
    
    Change-Id: I915d6deb6a3191850ce05a33609f462cf73170ef
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4357,8 +4357,64 @@ public final class Settings {
          */
         public static final String ALERT_ON_CHARGED_LEVEL = "alert_on_charged_level";
 
-        /** Whether to skip music track with volume rocker
         /**
+         * Whether to display sound panel in the power menu
+         *
+         * @hide
+         */
+        public static final String POWERMENU_SOUNDPANEL = "powermenu_soundpanel";
+
+        /**
+         * Whether to display screenshot in the power menu
+         *
+         * @hide
+         */
+        public static final String POWERMENU_SCREENSHOT = "powermenu_screenshot";
+
+        /**
+         * Whether to display settings in the power menu
+         *
+         * @hide
+         */
+        public static final String POWERMENU_SETTINGS = "powermenu_settings";
+
+        /**
+         * Whether to display lock in the power menu
+         *
+         * @hide
+         */
+        public static final String POWERMENU_LOCKDOWN = "powermenu_lockdown";
+
+        /**
+         * Whether to display airplane in the power menu
+         *
+         * @hide
+         */
+        public static final String POWERMENU_AIRPLANE = "powermenu_airplane";
+
+        /**
+         * Whether to display reboot in the power menu
+         *
+         * @hide
+         */
+        public static final String POWERMENU_RESTART = "powermenu_restart";
+
+        /**
+         * Whether to display power in the power menu
+         *
+         * @hide
+         */
+        public static final String POWERMENU_POWER = "powermenu_power";
+
+        /**
+         * Whether to display the users option in the power menu
+         *
+         * @hide
+         */
+        public static final String POWERMENU_USERS = "powermenu_users";
+
+        /**
+         * Whether to skip music track with volume rocker
          * @hide
          */
         public static final String VOLUME_BUTTON_MUSIC_CONTROL = "volume_button_music_control";
