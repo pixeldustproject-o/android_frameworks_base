66480ef9fff86ddc9c5711d5a275964eb7fcbafc
maxwen <max.weninger@gmail.com>
2017-10-08 01:42:30 +0200

    base: SystemUI: change disabled QS weather tile image
    Change-Id: I107ccabf655c7b9e099fac165cc46d88f009d3f7
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- core/java/com/android/internal/util/pixeldust/OmniJawsClient.java
+++ core/java/com/android/internal/util/pixeldust/OmniJawsClient.java
@@ -429,10 +429,6 @@ public class OmniJawsClient {
         }
     }
 
-    public Drawable getDefaultWeatherConditionImage() {
-        return mContext.getResources().getDrawable(com.android.internal.R.drawable.ic_qs_weather_default_on);
-    }
-
     public void addObserver(OmniJawsObserver observer) {
         if (mObserver.size() == 0) {
             if (mReceiver != null) {
