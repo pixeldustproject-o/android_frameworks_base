37443f649f189cd1ca5c513e592f6e2cb6701f64
Marko Man <darkobas@gmail.com>
2017-10-13 16:48:20 +0200

    PiP tile: add toast if pip mode is not available
    Change-Id: Idfa864c7b26f7965afb233ae377b4a520da326a8
    
    PD Edits: move custom_strings into pd_strings.
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- core/java/com/android/internal/util/pixeldust/DeviceUtils.java
+++ core/java/com/android/internal/util/pixeldust/DeviceUtils.java
@@ -17,11 +17,13 @@ package com.android.internal.util.pixeldust;
 
 import android.app.ActivityManagerNative;
 import android.app.IActivityManager;
+import android.app.ActivityManager;
 import android.app.ActivityManager.RunningAppProcessInfo;
 import android.bluetooth.BluetoothAdapter;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
@@ -278,5 +280,21 @@ public class DeviceUtils {
             // Do nothing; just let it go.
         }
         return targetKilled;
+    }
+
+    public static ActivityInfo getRunningActivityInfo(Context context) {
+        final ActivityManager am = (ActivityManager) context
+                .getSystemService(Context.ACTIVITY_SERVICE);
+        final PackageManager pm = context.getPackageManager();
+
+        List<ActivityManager.RunningTaskInfo> tasks = am.getRunningTasks(1);
+        if (tasks != null && !tasks.isEmpty()) {
+            ActivityManager.RunningTaskInfo top = tasks.get(0);
+            try {
+                return pm.getActivityInfo(top.topActivity, 0);
+            } catch (PackageManager.NameNotFoundException e) {
+            }
+        }
+        return null;
     }
 }
