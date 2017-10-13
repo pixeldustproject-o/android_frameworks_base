67e4c9a094d66b16fadb84f9e0b8cf2780c81165
Marko Man <darkobas@gmail.com>
2017-10-13 16:48:20 +0200

    PiP tile: add toast if pip mode is not available
    Change-Id: Idfa864c7b26f7965afb233ae377b4a520da326a8
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- core/java/com/android/internal/util/pixeldust/PixeldustUtils.java
+++ core/java/com/android/internal/util/pixeldust/PixeldustUtils.java
@@ -15,8 +15,10 @@
  */
 package com.android.internal.util.pixeldust;
 
+import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.hardware.input.InputManager;
 import android.os.Handler;
@@ -32,6 +34,8 @@ import android.view.IWindowManager;
 import android.view.WindowManagerGlobal;
 
 import com.android.internal.statusbar.IStatusBarService;
+
+import java.util.List;
 
 /**
  * Some custom utilities
@@ -112,5 +116,21 @@ public class PixeldustUtils {
                 }
             }
         }
+    }
+
+    public static ActivityInfo getRunningActivityInfo(Context context) {
+        final ActivityManager am = (ActivityManager) context
+                .getSystemService(Context.ACTIVITY_SERVICE);
+        final PackageManager pm = context.getPackageManager();
+
+        List<ActivityManager.RunningTaskInfo> tasks = am.getRunningTasks(1);
+        if (tasks != null && !tasks.isEmpty()) {
+            ActivityManager.RunningTaskInfo top = tasks.get(0);
+            try {
+                return pm.getActivityInfo(top.topActivity, 0);
+            } catch (PackageManager.NameNotFoundException e) {
+            }
+        }
+        return null;
     }
 }
