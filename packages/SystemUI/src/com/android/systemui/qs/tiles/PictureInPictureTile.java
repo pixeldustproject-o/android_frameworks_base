BenzoRoms/frameworks_base

QSTile: Add PiP tile
atm this works also with YouTube without Red subscription

Change-Id: Ib572e76a71228c4597e84213293051074723f89a
Signed-off-by: mydongistiny <jaysonedson@gmail.com>

packages/SystemUI/src/com/android/systemui/qs/tiles/PictureInPictureTile.java
@@ -1,0 +1,79 @@
+/*
+ * Copyright (C) 2017 ABC rom
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.qs.tiles;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.service.quicksettings.Tile;
+
+import com.android.internal.util.benzo.DeviceUtils;
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.systemui.qs.QSHost;
+import com.android.systemui.plugins.qs.QSTile.BooleanState;
+import com.android.systemui.qs.tileimpl.QSTileImpl;
+import com.android.systemui.R;
+
+/** Quick settings tile: PictureInPictureTile **/
+public class PictureInPictureTile extends QSTileImpl<BooleanState> {
+
+    public PictureInPictureTile(QSHost host) {
+        super(host);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.BENZO;
+    }
+
+    @Override
+    public BooleanState newTileState() {
+        return new BooleanState();
+    }
+
+    @Override
+    public void setListening(boolean listening) {}
+
+    @Override
+    public void handleClick() {
+        mHost.collapsePanels();
+        DeviceUtils.sendKeycode(171);
+    }
+
+    @Override
+    public void handleLongClick() {
+        //
+    }
+
+    @Override
+    public Intent getLongClickIntent() {
+        return null;
+    }
+
+    @Override
+    public CharSequence getTileLabel() {
+        return mContext.getString(R.string.quick_settings_pip_label);
+    }
+
+    @Override
+    protected void handleUpdateState(BooleanState state, Object arg) {
+        state.label = mContext.getString(R.string.quick_settings_pip_label);
+        state.icon = ResourceIcon.get(R.drawable.ic_qs_pip);
+        state.contentDescription =  mContext.getString(
+                R.string.quick_settings_pip_label);
+    }
+}

pixeldustproject-o/android_frameworks_base

Signed-off-by: spezi77 <spezi77@gmx.com>

packages/SystemUI/src/com/android/systemui/qs/tiles/PictureInPictureTile.java
@@ -1,0 +1,79 @@
+/*
+ * Copyright (C) 2017 ABC rom
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.qs.tiles;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.service.quicksettings.Tile;
+
+import com.android.internal.util.pixeldust.DeviceUtils;
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.systemui.qs.QSHost;
+import com.android.systemui.plugins.qs.QSTile.BooleanState;
+import com.android.systemui.qs.tileimpl.QSTileImpl;
+import com.android.systemui.R;
+
+/** Quick settings tile: PictureInPictureTile **/
+public class PictureInPictureTile extends QSTileImpl<BooleanState> {
+
+    public PictureInPictureTile(QSHost host) {
+        super(host);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.PIXELDUST;
+    }
+
+    @Override
+    public BooleanState newTileState() {
+        return new BooleanState();
+    }
+
+    @Override
+    public void setListening(boolean listening) {}
+
+    @Override
+    public void handleClick() {
+        mHost.collapsePanels();
+        DeviceUtils.sendKeycode(171);
+    }
+
+    @Override
+    public void handleLongClick() {
+        //
+    }
+
+    @Override
+    public Intent getLongClickIntent() {
+        return null;
+    }
+
+    @Override
+    public CharSequence getTileLabel() {
+        return mContext.getString(R.string.quick_settings_pip_label);
+    }
+
+    @Override
+    protected void handleUpdateState(BooleanState state, Object arg) {
+        state.label = mContext.getString(R.string.quick_settings_pip_label);
+        state.icon = ResourceIcon.get(R.drawable.ic_qs_pip);
+        state.contentDescription =  mContext.getString(
+                R.string.quick_settings_pip_label);
+    }
+}