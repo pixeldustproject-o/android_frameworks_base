c4e3a45b584ea0dfa38dacfb40f53ae48824dca6
ezio84 <brabus84@gmail.com>
2017-11-10 22:59:11 +0100

    Alert when screen off and charging if charge level becomes good [1/2]
    Don't alert if screen is on, or it's been off since less than one
    minute, or if battery isn't charging.
    
    Show a notification (good e.g. for Ambient display if you don't hear the sound).
    Play a sound if ringer mode is normal, otherwise vibrate if ringer mode is vibrate,
    otherwise nothing.
    
    Notification and sound/vibrate code are separated so the user can even
    disable the notification (long tap on it) but stil keep sound/vibrate alert.
    
    Thanks to xdnax for the nice idea and suggestions
    
    Change-Id: I1c300ba94ac01ab42dd2585144830727b4da092d
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- packages/SystemUI/src/com/android/systemui/power/PowerUI.java
+++ packages/SystemUI/src/com/android/systemui/power/PowerUI.java
@@ -99,6 +99,9 @@ public class PowerUI extends SystemUI {
         resolver.registerContentObserver(Settings.Global.getUriFor(
                 Settings.Global.LOW_POWER_MODE_TRIGGER_LEVEL),
                 false, obs, UserHandle.USER_ALL);
+        resolver.registerContentObserver(Settings.System.getUriFor(
+                Settings.System.ALERT_ON_CHARGED_LEVEL),
+                false, obs, UserHandle.USER_ALL);
         updateBatteryWarningLevels();
         mReceiver.init();
 
@@ -140,6 +143,10 @@ public class PowerUI extends SystemUI {
         mLowBatteryAlertCloseLevel = mLowBatteryReminderLevels[0]
                 + mContext.getResources().getInteger(
                         com.android.internal.R.integer.config_lowBatteryCloseWarningBump);
+
+        int level = Settings.System.getIntForUser(resolver,
+                Settings.System.ALERT_ON_CHARGED_LEVEL, -1, UserHandle.USER_CURRENT);
+        mWarnings.setChargedLevel(level);
     }
 
     /**
@@ -422,6 +429,8 @@ public class PowerUI extends SystemUI {
         void showThermalShutdownWarning();
         void dump(PrintWriter pw);
         void userSwitched();
+        void setChargedLevel(int level);
+        void setPluggedState(boolean plugged);
     }
 }
 
