1576c705a566394247a1caab89f0cce803aa67e1
maxwen <max.weninger@gmail.com>
2017-12-15 19:55:46 +0100

    [1/2] base: configure SystemUI theme
    allow to force light or dark or use the wallpaper based
    automatic selection
    
    Change-Id: Id7c287ea82cf76e6d3189c5621258cefe2c4d6d2
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4386,6 +4386,14 @@ public final class Settings {
         public static final String RECENTS_ICON_PACK = "recents_icon_pack";
 
         /**
+         * 0 - wallpaper based
+         * 1 - force light
+         * 2 - force dark
+         * @hide
+         */
+        public static final String SYSTEM_UI_THEME = "system_ui_theme";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
