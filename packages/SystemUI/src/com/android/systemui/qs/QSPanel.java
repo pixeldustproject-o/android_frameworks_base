fffdcb94c7b061f3ce5dd00dc46c238c38939ab8
spezi77 <spezi77@gmx.com>
2018-01-07 00:03:15 +0100

    Revert "Add auto brightness toggle to Quick Settings"
    This reverts commit d10926fb65f512b38cc56a2f70d57e0e6033d3ef.

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -98,8 +98,6 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
 
         mBrightnessView = LayoutInflater.from(context).inflate(
                 R.layout.quick_settings_brightness_dialog, this, false);
-        ImageView brightnessIcon = (ImageView) mBrightnessView.findViewById(R.id.brightness_icon);
-        brightnessIcon.setVisibility(View.VISIBLE);
         addView(mBrightnessView);
 
         setupTileLayout();
@@ -119,7 +117,7 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
         updateResources();
 
         mBrightnessController = new BrightnessController(getContext(),
-                brightnessIcon,
+                findViewById(R.id.brightness_icon),
                 findViewById(R.id.brightness_slider));
     }
 
