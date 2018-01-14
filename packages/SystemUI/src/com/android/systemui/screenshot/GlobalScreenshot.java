d630be4193c1519d4b8d6c82976fe70aad7f8295
Kilian von Pflugk <gerrit@jumoog.io>
2018-01-14 14:47:41 +0100

    App screenshot filename: keep compatibility with Cyrillic
    Change-Id: I6ba5660970814afba2a1ff061e67a0cf4897b658
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
@@ -164,7 +164,8 @@ class SaveImageInBackgroundTask extends AsyncTask<Void, Void, Void> {
         if (info != null) {
             CharSequence appName = pm.getApplicationLabel(info.applicationInfo);
             if (appName != null) {
-                String appNameString = appName.toString().replaceAll("\\s+", "_");
+                // replace all spaces and special chars with an underscore
+                String appNameString = appName.toString().replaceAll("[\\\\/:*?\"<>|]", "_");
                 mImageFileName = String.format(SCREENSHOT_FILE_NAME_TEMPLATE_APPNAME, appNameString, imageDate);
             }
         }
