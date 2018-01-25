e55c299b7069b52a4c3744ddcc8efbae11937517
maxwen <max.weninger@gmail.com>
2018-01-25 15:03:47 +0100

    Screenrecord tile: save quality level
    save state set in qs tile into settings
    
    Change-Id: I708b324a22c3e605fa6411c1a9e1de28b584938b
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4484,6 +4484,11 @@ public final class Settings {
         public static final String SHOW_FOURG = "show_fourg";
 
         /**
+         * @hide
+         */
+        public static final String SCREENRECORD_QUALITY_MODE = "screenrecord_quality_mode";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
