3081b231ca6979a956abd58d14e7c0f5e25f15b6
maxwen <max.weninger@gmail.com>
2018-02-18 18:35:22 -0500

    BatteryMeterDrawable: add square style (1/2)
    This was based off Omni's BatteryDroidView
    https://github.com/omnirom/android_frameworks_base/blob/android-8.1/packages/SystemUI/src/com/android/systemui/omni/BatteryDroidView.java
    
    Change-Id: I3f6ead99ebf74c8e216b238669026dfdbf03423b
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -8432,7 +8432,7 @@ public final class Settings {
         /**
          * Display style of the status bar battery information
          * 0: Display the battery an icon in portrait mode
-         * 1: Display the battery an icon in landscape mode
+         * 1: Display the battery as a square
          * 2: Display the battery as a circle
          * 3: Display the battery as a dotted circle
          * 4: Display the battery as a big circle and show level into the icon
