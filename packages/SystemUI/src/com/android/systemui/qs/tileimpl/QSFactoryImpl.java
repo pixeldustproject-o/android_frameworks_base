21805fac548d8c7a962053abaad603975f98331f
Adin Kwok <adin.kwok@carbonrom.org>
2018-04-07 20:08:45 -0700

    SystemUI: Add Smart Pixels tile
    Single tap enables/disables.
    Long press opens Smart Pixels settings.
    
    User is not allowed to enable or disable when
    Smart Pixels has been auto-enabled on battery saver.
    
    Change-Id: I535c84a0ca6360e1db351391e9d0fb9095896ee3
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -53,6 +53,7 @@ import com.android.systemui.qs.tiles.ScreenshotTile;
 import com.android.systemui.qs.tiles.VolumeTile;
 import com.android.systemui.qs.tiles.SyncTile;
 import com.android.systemui.qs.tiles.UsbTetherTile;
+import com.android.systemui.qs.tiles.SmartPixelsTile;
 import com.android.systemui.qs.tiles.ThemeTile;
 import com.android.systemui.qs.tiles.UserTile;
 import com.android.systemui.qs.tiles.WeatherTile;
@@ -105,6 +106,7 @@ public class QSFactoryImpl implements QSFactory {
         else if (tileSpec.equals("pixeldust_settings")) return new PDSettingsTile(mHost);
         else if (tileSpec.equals("theme")) return new ThemeTile(mHost);
         else if (tileSpec.equals("expanded_desktop")) return new ExpandedDesktopTile(mHost);
+        else if (tileSpec.equals("smartpixels")) return new SmartPixelsTile(mHost);
         // Intent tiles.
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(mHost, tileSpec);
         else if (tileSpec.startsWith(CustomTile.PREFIX)) return CustomTile.create(mHost, tileSpec);
