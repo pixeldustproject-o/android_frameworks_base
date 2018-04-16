0ce272fafe7bf96e5bcf4198e2349a36b703b50d
wangmingming1 <wangmingming1@xiaomi.com>
2018-04-16 15:21:53 +0800

    Don't count apk size if it's already staged
    fix installation failure when installing a veriy big app
    with low disk usable space
    
    Test: Manual
    
    Change-Id: I0074e57073dd98fff37f482923527a750b950a4a
    Signed-off-by: wangmingming1 <wangmingming1@xiaomi.com>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -9128,7 +9128,7 @@ public class PackageManagerService extends IPackageManager.Stub
         int fileCount = 0;
         for (File file : files) {
             final boolean isPackage = (isApkFile(file) || file.isDirectory())
-                    && !PackageInstallerService.isStageName(file.getName());
+                    && !PackageHelper.isStageName(file.getName());
             if (!isPackage) {
                 // Ignore entries which are not packages
                 continue;
@@ -23513,7 +23513,7 @@ Slog.v(TAG, ":: stepped forward, applying functor at tag " + parser.getName());
             synchronized (mPackages) {
                 for (String cid : list) {
                     // Leave stages untouched for now; installer service owns them
-                    if (PackageInstallerService.isStageName(cid)) continue;
+                    if (PackageHelper.isStageName(cid)) continue;
 
                     if (DEBUG_SD_INSTALL)
                         Log.i(TAG, "Processing container " + cid);
@@ -24025,7 +24025,7 @@ Slog.v(TAG, ":: stepped forward, applying functor at tag " + parser.getName());
                 Environment.getDataAppDirectory(volumeUuid));
         for (File file : files) {
             final boolean isPackage = (isApkFile(file) || file.isDirectory())
-                    && !PackageInstallerService.isStageName(file.getName());
+                    && !PackageHelper.isStageName(file.getName());
             if (!isPackage) {
                 // Ignore entries which are not packages
                 continue;
