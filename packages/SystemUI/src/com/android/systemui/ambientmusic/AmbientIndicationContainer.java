465fa6dcab5727dd02e8f03a29235f653de048f5
ezio84 <brabus84@gmail.com>
2018-04-30 20:30:22 +0200

    Ambient music fallback: be sure to remove track length info
    if you play a track with available metadata and length info, then
    play a track without them (e.g. kdeconnect or spotify remote session)
    the old track length info is still showing
    
    Change-Id: I8cd62db9a2d1e8211f0be70e97ed942e8c08e956
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- packages/SystemUI/src/com/android/systemui/ambientmusic/AmbientIndicationContainer.java
+++ packages/SystemUI/src/com/android/systemui/ambientmusic/AmbientIndicationContainer.java
@@ -136,6 +136,7 @@ public class AmbientIndicationContainer extends AutoReinflateContainer {
             mInfoToSet = charSequence.toString();
         } else if (!TextUtils.isEmpty(notificationText)) {
             mInfoToSet = notificationText;
+            mLengthInfo = null;
         }
 
         mInfoAvailable = mInfoToSet != null;
