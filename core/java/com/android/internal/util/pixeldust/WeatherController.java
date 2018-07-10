BenzoRoms/frameworks_base

Keyguard: Create lockscreen weather
*also includes "SystemUI: Remove redundant and broken setText on temperture view."
https://github.com/Euphoria-OS-Legacy/android_frameworks_base/commit/e0875874c4b45c1619f5a2fe369092079e1ecd94

Change-Id: Ibf3fb8c877470ae152b778c9dc7bbf9aa6ae5f10
Signed-off-by: mydongistiny <jaysonedson@gmail.com>

core/java/com/android/internal/util/benzo/WeatherController.java
@@ -1,0 +1,36 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.util.benzo;
+
+import android.graphics.drawable.Drawable;
+
+public interface WeatherController {
+    void addCallback(Callback callback);
+    void removeCallback(Callback callback);
+    WeatherInfo getWeatherInfo();
+
+    public interface Callback {
+        void onWeatherChanged(WeatherInfo temp);
+    }
+    public static class WeatherInfo {
+        public String temp = null;
+        public String city = null;
+        public String condition = null;
+        public int conditionCode = 0;
+        public Drawable conditionDrawable = null;
+    }
+}

core/java/com/android/internal/util/benzo/WeatherControllerImpl.java
@@ -1,0 +1,140 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.util.bemzo;
+
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.database.Cursor;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+public class WeatherControllerImpl implements WeatherController {
+
+    private static final String TAG = WeatherController.class.getSimpleName();
+    private static final boolean DEBUG = Log.isLoggable(TAG, Log.DEBUG);
+
+    public static final ComponentName COMPONENT_WEATHER_FORECAST = new ComponentName(
+            "com.cyanogenmod.lockclock", "com.cyanogenmod.lockclock.weather.ForecastActivity");
+    public static final String ACTION_UPDATE_FINISHED
+            = "com.cyanogenmod.lockclock.action.WEATHER_UPDATE_FINISHED";
+    public static final String EXTRA_UPDATE_CANCELLED = "update_cancelled";
+    public static final String ACTION_FORCE_WEATHER_UPDATE
+            = "com.cyanogenmod.lockclock.action.FORCE_WEATHER_UPDATE";
+    public static final Uri CURRENT_WEATHER_URI
+            = Uri.parse("content://com.cyanogenmod.lockclock.weather.provider/weather/current");
+    public static final String[] WEATHER_PROJECTION = new String[]{
+            "temperature",
+            "city",
+            "condition",
+            "condition_code"
+    };
+    public static final String LOCK_CLOCK_PACKAGE_NAME = "com.cyanogenmod.lockclock";
+
+    private final ArrayList<Callback> mCallbacks = new ArrayList<Callback>();
+    private final Receiver mReceiver = new Receiver();
+    private final Context mContext;
+
+    private WeatherInfo mCachedInfo = new WeatherInfo();
+
+    public WeatherControllerImpl(Context context) {
+        mContext = context;
+                mContext.getSystemService(Context.CONNECTIVITY_SERVICE);
+        queryWeather();
+        final IntentFilter filter = new IntentFilter();
+        filter.addAction(ACTION_UPDATE_FINISHED);
+        mContext.registerReceiver(mReceiver, filter);
+    }
+
+    public void addCallback(Callback callback) {
+        if (callback == null || mCallbacks.contains(callback)) return;
+        if (DEBUG) Log.d(TAG, "addCallback " + callback);
+        mCallbacks.add(callback);
+        callback.onWeatherChanged(mCachedInfo); // immediately update with current values
+    }
+
+    public void removeCallback(Callback callback) {
+        if (callback == null) return;
+        if (DEBUG) Log.d(TAG, "removeCallback " + callback);
+        mCallbacks.remove(callback);
+    }
+
+    private Drawable getIcon(int conditionCode) {
+        try {
+            Resources resources =
+                    mContext.createPackageContext(LOCK_CLOCK_PACKAGE_NAME, 0).getResources();
+            return resources.getDrawable(resources.getIdentifier("weather_" + conditionCode,
+                    "drawable", LOCK_CLOCK_PACKAGE_NAME));
+        } catch (PackageManager.NameNotFoundException e) {
+            return null;
+        }
+    }
+
+    @Override
+    public WeatherInfo getWeatherInfo() {
+        return mCachedInfo;
+    }
+
+    private void queryWeather() {
+        Cursor c = mContext.getContentResolver().query(CURRENT_WEATHER_URI, WEATHER_PROJECTION,
+                null, null, null);
+        if (c == null) {
+            if(DEBUG) Log.e(TAG, "cursor was null for temperature, forcing weather update");
+            mContext.sendBroadcast(new Intent(ACTION_FORCE_WEATHER_UPDATE));
+        } else {
+            try {
+                c.moveToFirst();
+                mCachedInfo.temp = c.getString(0);
+                mCachedInfo.city = c.getString(1);
+                mCachedInfo.condition = c.getString(2);
+                mCachedInfo.conditionCode = c.getInt(3);
+                mCachedInfo.conditionDrawable = getIcon(mCachedInfo.conditionCode);
+            } finally {
+                c.close();
+            }
+        }
+    }
+
+    private void fireCallback() {
+        for (Callback callback : mCallbacks) {
+            callback.onWeatherChanged(mCachedInfo);
+        }
+    }
+
+    private final class Receiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (DEBUG) Log.d(TAG, "onReceive " + intent.getAction());
+            if (intent.hasExtra(EXTRA_UPDATE_CANCELLED)) {
+                if (intent.getBooleanExtra(EXTRA_UPDATE_CANCELLED, false)) {
+                    // no update
+                    return;
+                }
+            }
+            queryWeather();
+            fireCallback();
+        }
+    }
+
+}

packages/Keyguard/src/com/android/keyguard/KeyguardStatusView.java
@@ -18,7 +18,9 @@ package com.android.keyguard;
 
 import android.app.ActivityManager;
 import android.app.AlarmManager;
+import android.content.ContentResolver;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.content.res.ColorStateList;
 import android.content.res.Configuration;
 import android.content.res.Resources;
@@ -41,24 +43,32 @@ import android.widget.ImageView;
 import android.widget.TextClock;
 import android.widget.TextView;
 
+import com.android.internal.util.benzo.WeatherController;
+import com.android.internal.util.benzo.WeatherControllerImpl;
 import com.android.internal.widget.LockPatternUtils;
 
 import java.util.Date;
 import java.text.NumberFormat;
 import java.util.Locale;
 
-public class KeyguardStatusView extends GridLayout {
+public class KeyguardStatusView extends GridLayout implements
+        WeatherController.Callback  {
     private static final boolean DEBUG = KeyguardConstants.DEBUG;
     private static final String TAG = "KeyguardStatusView";
 
     private final LockPatternUtils mLockPatternUtils;
     private final AlarmManager mAlarmManager;
 
+    private ImageView mWeatherIcon;
     private TextView mAlarmStatusView;
     private TextClock mDateView;
     private TextClock mClockView;
     private TextView mAmbientDisplayBatteryView;
     private TextView mOwnerInfo;
+    private TextView mTemperatureText;
+    private TextView mWeatherCity;
+
+    private WeatherController mWeatherController;
 
     //On the first boot, keyguard will start to receiver TIME_TICK intent.
     //And onScreenTurnedOff will not get called if power off when keyguard is not started.
@@ -119,6 +129,7 @@ public class KeyguardStatusView extends GridLayout {
         super(context, attrs, defStyle);
         mAlarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         mLockPatternUtils = new LockPatternUtils(getContext());
+        mWeatherController = new WeatherControllerImpl(mContext);
     }
 
     private void setEnableMarquee(boolean enabled) {
@@ -137,9 +148,11 @@ public class KeyguardStatusView extends GridLayout {
         mClockView.setShowCurrentUserTime(true);
         mAmbientDisplayBatteryView = (TextView) findViewById(R.id.ambient_display_battery_view);
         mOwnerInfo = (TextView) findViewById(R.id.owner_info);
-
         boolean shouldMarquee = KeyguardUpdateMonitor.getInstance(mContext).isDeviceInteractive();
         setEnableMarquee(shouldMarquee);
+        mWeatherIcon = (ImageView) findViewById(R.id.weather_image);
+        mWeatherCity = (TextView) findViewById(R.id.city);
+        mTemperatureText = (TextView) findViewById(R.id.temperature);
         refresh();
         updateOwnerInfo();
 
@@ -201,6 +214,7 @@ public class KeyguardStatusView extends GridLayout {
         refreshAlarmStatus(nextAlarm);
         hideLockscreenItems();
         refreshLockFont();
+        updateWeatherSettings(false);
     }
 
     void refreshAlarmStatus(AlarmManager.AlarmClockInfo nextAlarm) {
@@ -242,12 +256,14 @@ public class KeyguardStatusView extends GridLayout {
         super.onAttachedToWindow();
         KeyguardUpdateMonitor.getInstance(mContext).registerCallback(mInfoCallback);
         hideLockscreenItems();
+        mWeatherController.addCallback(this);
     }
 
     @Override
     protected void onDetachedFromWindow() {
         super.onDetachedFromWindow();
         KeyguardUpdateMonitor.getInstance(mContext).removeCallback(mInfoCallback);
+        mWeatherController.removeCallback(this);
     }
 
     private String getOwnerInfo() {
@@ -398,6 +414,19 @@ public class KeyguardStatusView extends GridLayout {
         return Settings.System.getInt(getContext().getContentResolver(),
                 Settings.System.AMBIENT_DISPLAY_SHOW_BATTERY, 1) == 1;
     } 
+
+    @Override
+    public void onWeatherChanged(WeatherController.WeatherInfo info) {
+        if (info.temp == null || info.condition == null) {
+            mTemperatureText.setText(null);
+            mWeatherCity.setText("--");
+            mWeatherIcon.setImageDrawable(null);
+        } else {
+            mTemperatureText.setText(info.temp);
+            mWeatherCity.setText(info.city);
+            mWeatherIcon.setImageDrawable(info.conditionDrawable);
+        }
+    }
 
     // DateFormat.getBestDateTimePattern is extremely expensive, and refresh is called often.
     // This is an optimization to ensure we only recompute the patterns when the inputs change.

pixeldustproject-o/android_frameworks_base

Signed-off-by: spezi77 <spezi77@gmx.com>

core/java/com/android/internal/util/pixeldust/WeatherController.java
@@ -1,0 +1,36 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.util.pixeldust;
+
+import android.graphics.drawable.Drawable;
+
+public interface WeatherController {
+    void addCallback(Callback callback);
+    void removeCallback(Callback callback);
+    WeatherInfo getWeatherInfo();
+
+    public interface Callback {
+        void onWeatherChanged(WeatherInfo temp);
+    }
+    public static class WeatherInfo {
+        public String temp = null;
+        public String city = null;
+        public String condition = null;
+        public int conditionCode = 0;
+        public Drawable conditionDrawable = null;
+    }
+}

core/java/com/android/internal/util/pixeldust/WeatherControllerImpl.java
@@ -1,0 +1,140 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.util.pixeldust;
+
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.database.Cursor;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+public class WeatherControllerImpl implements WeatherController {
+
+    private static final String TAG = WeatherController.class.getSimpleName();
+    private static final boolean DEBUG = Log.isLoggable(TAG, Log.DEBUG);
+
+    public static final ComponentName COMPONENT_WEATHER_FORECAST = new ComponentName(
+            "com.cyanogenmod.lockclock", "com.cyanogenmod.lockclock.weather.ForecastActivity");
+    public static final String ACTION_UPDATE_FINISHED
+            = "com.cyanogenmod.lockclock.action.WEATHER_UPDATE_FINISHED";
+    public static final String EXTRA_UPDATE_CANCELLED = "update_cancelled";
+    public static final String ACTION_FORCE_WEATHER_UPDATE
+            = "com.cyanogenmod.lockclock.action.FORCE_WEATHER_UPDATE";
+    public static final Uri CURRENT_WEATHER_URI
+            = Uri.parse("content://com.cyanogenmod.lockclock.weather.provider/weather/current");
+    public static final String[] WEATHER_PROJECTION = new String[]{
+            "temperature",
+            "city",
+            "condition",
+            "condition_code"
+    };
+    public static final String LOCK_CLOCK_PACKAGE_NAME = "com.cyanogenmod.lockclock";
+
+    private final ArrayList<Callback> mCallbacks = new ArrayList<Callback>();
+    private final Receiver mReceiver = new Receiver();
+    private final Context mContext;
+
+    private WeatherInfo mCachedInfo = new WeatherInfo();
+
+    public WeatherControllerImpl(Context context) {
+        mContext = context;
+                mContext.getSystemService(Context.CONNECTIVITY_SERVICE);
+        queryWeather();
+        final IntentFilter filter = new IntentFilter();
+        filter.addAction(ACTION_UPDATE_FINISHED);
+        mContext.registerReceiver(mReceiver, filter);
+    }
+
+    public void addCallback(Callback callback) {
+        if (callback == null || mCallbacks.contains(callback)) return;
+        if (DEBUG) Log.d(TAG, "addCallback " + callback);
+        mCallbacks.add(callback);
+        callback.onWeatherChanged(mCachedInfo); // immediately update with current values
+    }
+
+    public void removeCallback(Callback callback) {
+        if (callback == null) return;
+        if (DEBUG) Log.d(TAG, "removeCallback " + callback);
+        mCallbacks.remove(callback);
+    }
+
+    private Drawable getIcon(int conditionCode) {
+        try {
+            Resources resources =
+                    mContext.createPackageContext(LOCK_CLOCK_PACKAGE_NAME, 0).getResources();
+            return resources.getDrawable(resources.getIdentifier("weather_" + conditionCode,
+                    "drawable", LOCK_CLOCK_PACKAGE_NAME));
+        } catch (PackageManager.NameNotFoundException e) {
+            return null;
+        }
+    }
+
+    @Override
+    public WeatherInfo getWeatherInfo() {
+        return mCachedInfo;
+    }
+
+    private void queryWeather() {
+        Cursor c = mContext.getContentResolver().query(CURRENT_WEATHER_URI, WEATHER_PROJECTION,
+                null, null, null);
+        if (c == null) {
+            if(DEBUG) Log.e(TAG, "cursor was null for temperature, forcing weather update");
+            mContext.sendBroadcast(new Intent(ACTION_FORCE_WEATHER_UPDATE));
+        } else {
+            try {
+                c.moveToFirst();
+                mCachedInfo.temp = c.getString(0);
+                mCachedInfo.city = c.getString(1);
+                mCachedInfo.condition = c.getString(2);
+                mCachedInfo.conditionCode = c.getInt(3);
+                mCachedInfo.conditionDrawable = getIcon(mCachedInfo.conditionCode);
+            } finally {
+                c.close();
+            }
+        }
+    }
+
+    private void fireCallback() {
+        for (Callback callback : mCallbacks) {
+            callback.onWeatherChanged(mCachedInfo);
+        }
+    }
+
+    private final class Receiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (DEBUG) Log.d(TAG, "onReceive " + intent.getAction());
+            if (intent.hasExtra(EXTRA_UPDATE_CANCELLED)) {
+                if (intent.getBooleanExtra(EXTRA_UPDATE_CANCELLED, false)) {
+                    // no update
+                    return;
+                }
+            }
+            queryWeather();
+            fireCallback();
+        }
+    }
+
+}

packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
@@ -18,7 +18,9 @@ package com.android.keyguard;
 
 import android.app.ActivityManager;
 import android.app.AlarmManager;
+import android.content.ContentResolver;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.content.res.ColorStateList;
 import android.content.res.Configuration;
 import android.content.res.Resources;
@@ -41,6 +43,8 @@ import android.widget.TextClock;
 import android.widget.TextView;
 
 import com.android.internal.util.ArrayUtils;
+import com.android.internal.util.pixeldust.WeatherController;
+import com.android.internal.util.pixeldust.WeatherControllerImpl;
 import com.android.internal.widget.LockPatternUtils;
 import com.android.systemui.ChargingView;
 import com.android.systemui.doze.DozeLog;
@@ -48,7 +52,8 @@ import com.android.systemui.statusbar.policy.DateView;
 
 import java.util.Locale;
 
-public class KeyguardStatusView extends GridLayout {
+public class KeyguardStatusView extends GridLayout implements
+        WeatherController.Callback  {
     private static final boolean DEBUG = KeyguardConstants.DEBUG;
     private static final String TAG = "KeyguardStatusView";
     private static final int MARQUEE_DELAY_MS = 2000;
@@ -56,6 +61,7 @@ public class KeyguardStatusView extends GridLayout {
     private final LockPatternUtils mLockPatternUtils;
     private final AlarmManager mAlarmManager;
 
+    private ImageView mWeatherIcon;
     private TextView mAlarmStatusView;
     private DateView mDateView;
     private TextClock mClockView;
@@ -65,6 +71,10 @@ public class KeyguardStatusView extends GridLayout {
     private View mKeyguardStatusArea;
     private Runnable mPendingMarqueeStart;
     private Handler mHandler;
+    private TextView mTemperatureText;
+    private TextView mWeatherCity;
+
+    private WeatherController mWeatherController;
 
     private View[] mVisibleInDoze;
     private boolean mPulsing;
@@ -121,6 +131,7 @@ public class KeyguardStatusView extends GridLayout {
         mAlarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         mLockPatternUtils = new LockPatternUtils(getContext());
         mHandler = new Handler(Looper.myLooper());
+        mWeatherController = new WeatherControllerImpl(mContext);
     }
 
     private void setEnableMarquee(boolean enabled) {
@@ -169,6 +180,9 @@ public class KeyguardStatusView extends GridLayout {
 
         boolean shouldMarquee = KeyguardUpdateMonitor.getInstance(mContext).isDeviceInteractive();
         setEnableMarquee(shouldMarquee);
+        mWeatherIcon = (ImageView) findViewById(R.id.weather_image);
+        mWeatherCity = (TextView) findViewById(R.id.city);
+        mTemperatureText = (TextView) findViewById(R.id.temperature);
         refresh();
         updateOwnerInfo();
 
@@ -209,6 +223,7 @@ public class KeyguardStatusView extends GridLayout {
 
         refreshTime();
         refreshAlarmStatus(nextAlarm);
+        updateWeatherSettings(false);
     }
 
     void refreshAlarmStatus(AlarmManager.AlarmClockInfo nextAlarm) {
@@ -257,12 +272,14 @@ public class KeyguardStatusView extends GridLayout {
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
         KeyguardUpdateMonitor.getInstance(mContext).registerCallback(mInfoCallback);
+        mWeatherController.addCallback(this);
     }
 
     @Override
     protected void onDetachedFromWindow() {
         super.onDetachedFromWindow();
         KeyguardUpdateMonitor.getInstance(mContext).removeCallback(mInfoCallback);
+        mWeatherController.removeCallback(this);
     }
 
     private String getOwnerInfo() {
@@ -285,6 +302,20 @@ public class KeyguardStatusView extends GridLayout {
     @Override
     public boolean hasOverlappingRendering() {
         return false;
+    }
+
+ 
+    @Override
+    public void onWeatherChanged(WeatherController.WeatherInfo info) {
+        if (info.temp == null || info.condition == null) {
+            mTemperatureText.setText(null);
+            mWeatherCity.setText("--");
+            mWeatherIcon.setImageDrawable(null);
+        } else {
+            mTemperatureText.setText(info.temp);
+            mWeatherCity.setText(info.city);
+            mWeatherIcon.setImageDrawable(info.conditionDrawable);
+        }
     }
 
     // DateFormat.getBestDateTimePattern is extremely expensive, and refresh is called often.