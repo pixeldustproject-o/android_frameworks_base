f71816b1b074dc8b9bc148275280b6e791cf6fa6
spezi77 <spezi7713@gmx.net>
2018-07-23 01:16:29 +0200

    Revert "Fix InCallActivity onStop call when launch from Lockscreen"
    This reverts commit 282d9ba02c816a05a6ed7eed018ec2f8bbfb9b1f.
    
    This commit has introduced an issue which prevents the kernel to enter deep sleep mode.
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -12546,7 +12546,7 @@ public class ActivityManagerService extends IActivityManager.Stub
     }
 
     boolean isSleepingLocked() {
-        return mSleeping && mWakefulness == PowerManagerInternal.WAKEFULNESS_ASLEEP;
+        return mSleeping;
     }
 
     void onWakefulnessChanged(int wakefulness) {
